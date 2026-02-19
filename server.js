a1a76cb2b4369809abe60f863c435f6bd0b96660
Pheromoneo <_@example.com>
2026-02-20 00:00:55 +0300

    Updated API logic and added random quote feature

--- server.js
+++ server.js
@@ -7,16 +7,22 @@ const port = 3000;
 // Обработчик для маршрута /api/quote
 app.get('/api/quote', async (req, res) => {
   try {
-    const response = await axios.get('https://gist.githubusercontent.com/Pheromoneo/6fcf69b4069ca396def39c52576e9b07/raw/6a9284f4570296ea2756959c337113d797353c70/gistfile1.txt');
+    // Получаем данные с Gist
+    const response = await axios.get('https://gist.githubusercontent.com/Pheromoneo/6fcf69b4069ca396def39c52576e9b07/raw/34fab88c183d5c4e3fd0426e8333d0e0865318f6/gistfile1.txt');
     
+    // Разбиваем текст на строки
     const quotes = response.data
       .split('\n')
       .map(q => q.trim())
       .filter(q => q.length > 0);
 
+    // Выбираем случайную цитату
     const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+    // Отправляем случайную цитату в ответ
     res.status(200).send(randomQuote);
   } catch (error) {
+    console.error("Error fetching quotes:", error);
     res.status(500).send('Error loading quotes.');
   }
 });
