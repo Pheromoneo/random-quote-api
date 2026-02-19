039f11357506c0f32ef6854f0ffc24f1e042a2a6
Pheromoneo <_@example.com>
2026-02-19 21:49:01 +0300

    Gist integration

---
+++ api/quote.js
@@ -0,0 +1,19 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch("https://gist.githubusercontent.com/Pheromoneo/6fcf69b4069ca396def39c52576e9b07/raw/6a9284f4570296ea2756959c337113d797353c70/gistfile1.txt");
+
+    const text = await response.text();
+
+    const quotes = text
+      .split("\n")
+      .map(q => q.trim())
+      .filter(q => q.length > 0);
+
+    const random = quotes[Math.floor(Math.random() * quotes.length)];
+
+    res.status(200).send(random);
+
+  } catch (error) {
+    res.status(500).send("Error loading quotes.");
+  }
+}
